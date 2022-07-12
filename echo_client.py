662ec1b759fd9fae7f74b98f8cb883165b84d689
Natalie Abramov <natalie.abramov7@gmail.com>
2022-07-12 18:18:55 -0400

    small echo client changes

--- echo_client.py
+++ echo_client.py
@@ -8,8 +8,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
     
     sent_msg = s.sendall(b'hi') #msg in byte form
-    data = s.recv(1024)
-    print(f"Received {data!r}")
+    data1 = s.recv(1024)
+    print(f"Received {data1!r}")
 
     msg = b'hi'
     message = msg.decode('utf-8') #makes msg string
@@ -19,11 +19,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     #send_length += b' ' * (HEADER - len(send_length)) #creates spaces to make fillers so first msg is size of header 
 
     s.sendall(send_length) #sends size of outgoing message
-    data = s.recv(1024)
+    data2 = s.recv(1024)
 
-    print(f"Received {data!r}")
+    print(f"Received {data2!r}")
 
     s.sendall(msg) #sends message
-    data = s.recv(1024)
+    data3 = s.recv(1024)
 
-    print(f"Received {data!r}")
+    print(f"Received {data3!r}")
