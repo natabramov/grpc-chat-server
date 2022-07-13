842aea7e312b9776540f98f75209bb5ec1fbb7f7
Natalie Abramov <natalie.abramov7@gmail.com>
2022-07-13 15:57:13 -0400

    Added indefinite messaging from client

--- echo_client.py
+++ echo_client.py
@@ -3,26 +3,43 @@ import socket
 HEADER = 20
 HOST = "127.0.0.1"
 PORT = 12345
+user_input = 'hi'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
     
-    msg = 'hi'.encode('utf-8')
-    send_length = len(msg).to_bytes(4, "big")
+    print("[Type 'Bye' to disconnect]")
+    while user_input.lower() != 'bye'.lower():
+        user_input = input("\nType a message: ")
+        
+        msg = user_input.encode('utf-8') #makes a str
+        send_length = len(msg).to_bytes(4, "big") #converts str to bytes
 
-    s.sendall(send_length) #sends size of outgoing message
-    s.sendall(msg)
-    data = s.recv(1024)
+        s.sendall(send_length) #sends size of outgoing message to server
+        s.sendall(msg) #sends msg to server 
+        data = s.recv(1024)
 
-    print(f"Received {data!r}")
+        print(f"Received {data!r}")
 
-    ####
 
-    msg2 = 'hello again'.encode('utf-8')
-    send_length2 = len(msg2).to_bytes(4, "big")
 
-    s.sendall(send_length2)
-    s.sendall(msg2)
-    data2 = s.recv(1024)
 
-    print(f"Received {data2!r}")
+    # msg = 'hi'.encode('utf-8') #makes a str
+    # send_length = len(msg).to_bytes(4, "big") #converts str to bytes
+
+    # s.sendall(send_length) #sends size of outgoing message to server
+    # s.sendall(msg) #sends msg to server 
+    # data = s.recv(1024)
+
+    # print(f"Received {data!r}")
+
+    # ###
+
+    # msg2 = 'BYE'.encode('utf-8')
+    # send_length2 = len(msg2).to_bytes(4, "big")
+
+    # s.sendall(send_length2)
+    # s.sendall(msg2)
+    # data2 = s.recv(1024)
+
+    # print(f"Received {data2!r}")
