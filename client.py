ef6061d64fe979337cf914b3222df7e55c67aa7d
Natalie Abramov <natalie.abramov7@gmail.com>
2022-07-18 18:15:42 -0400

    updated server and client

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import socket
 
-HEADER = 20
 HOST = "127.0.0.1"
 PORT = 12345
 user_input = 'hi'
@@ -20,26 +19,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         data = s.recv(1024).decode()
 
         print(f"Received {data!r}")
-
-
-
-
-    # msg = 'hi'.encode('utf-8') #makes a str
-    # send_length = len(msg).to_bytes(4, "big") #converts str to bytes
-
-    # s.sendall(send_length) #sends size of outgoing message to server
-    # s.sendall(msg) #sends msg to server 
-    # data = s.recv(1024)
-
-    # print(f"Received {data!r}")
-
-    # ###
-
-    # msg2 = 'BYE'.encode('utf-8')
-    # send_length2 = len(msg2).to_bytes(4, "big")
-
-    # s.sendall(send_length2)
-    # s.sendall(msg2)
-    # data2 = s.recv(1024)
-
-    # print(f"Received {data2!r}")
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import logging
 
-HEADER = 4
+HEADER = 16
 HOST = "127.0.0.1"
 PORT = 12345
 
